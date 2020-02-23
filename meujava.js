db28cdf310d232e0f4e761dfa9c4fc0e5e0f4ac4
alissonlucas02 <55361737+alissonlucas02@users.noreply.github.com>
2020-02-22 21:38:33 -0300

    Add files via upload

---
+++ meujava.js
@@ -0,0 +1,11 @@
+function testando() {
+	var texto = document.getElementById('texto').value;
+	var mensa;
+	if (texto == 'alisson') {
+		alert('meu nome é SR.ALISSON');
+	}
+	else {
+		alert('tanto faz amigo')
+	}
+}
+	
