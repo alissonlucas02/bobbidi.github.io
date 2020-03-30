89138cda55eb7a22beca6fd2604b030e9e7c826b
alissonlucas02 <55361737+alissonlucas02@users.noreply.github.com>
2020-03-29 22:50:03 -0300

    Add files via upload

--- js/java.js
+++ js/java.js
@@ -1,4 +1,9 @@
-function teste(){
-	var a = "“Tente de novo. Fracasse de novo. Mas fracasse melhor”";
-	document.getElementById('demo').innerHTML = a;
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
 }
